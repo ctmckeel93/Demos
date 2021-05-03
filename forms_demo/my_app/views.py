be2432eebd98a36278ddd8e129eb3e72211d34e2
Corey Mckeel <coreymckeel@Coreys-MacBook-Air.local>
2021-05-03 05:05:13 -0400

    added forms code

--- forms_demo/my_app/views.py
+++ forms_demo/my_app/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User 
+from .forms import RegisterForm, LoginForm 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    form = RegisterForm()
+    form2 = LoginForm()
+    context = {
+        "regForm" : form,
+        "logForm" : form2,
+    }
+    return render(request, 'index.html', context)
+
+def register(request):
+    fname = request.POST['first_name']
+    lname = request.POST['last_name']
+    email = request.POST['email']
+    password = request.POST['password']
+    user = User.objects.create(first_name=fname, last_name=lname, email=email, password=password)
+    user.save()
+    request.session['logged_in'] = user.id 
+    return redirect('/')
